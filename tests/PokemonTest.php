6d82f4907945140b7c6c6f7fcb9646aebe098552
Jeroen van den Enden <info@endroid.nl>
2023-05-13 17:12:23 +0000

    No tests yet

---
+++ tests/PokemonTest.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Endroid\Pokemon\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+final class PokemonTest extends TestCase
+{
+    /**
+     * @testdox No tests yet
+     */
+    public function testNoTestsYet()
+    {
+        $this->assertTrue(true);
+    }
+}
