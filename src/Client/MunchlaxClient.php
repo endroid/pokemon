277c7fbdd471e90e3a6171885f08ceb90093807f
Jeroen van den Enden <info@endroid.nl>
2023-05-31 22:34:42 +0200

    Filter events

--- src/Client/MunchlaxClient.php
+++ src/Client/MunchlaxClient.php
@@ -11,20 +11,37 @@ final class MunchlaxClient
 {
     public function getCalendar(): Calendar
     {
+        $skipTitles = ['Season '];
+        $skipCategories = ['Go Battle League', 'Research Breakthrough'];
+
         $crawler = new Crawler((string) file_get_contents('https://www.munchlax.nl/'));
-        $events = $crawler->filter('.list-group-item')->each(function (Crawler $node) {
+        $events = $crawler->filter('.list-group-item')->each(function (Crawler $node) use ($skipTitles, $skipCategories) {
             $title = $node->filter('.title')->innerText();
+            foreach ($skipTitles as $skipTitle) {
+                if (str_contains($title, $skipTitle)) {
+                    return null;
+                }
+            }
+            $category = $node->filter('.badge-primary')->innerText();
+            foreach ($skipCategories as $skipCategory) {
+                if (strtolower($skipCategory) === strtolower($category)) {
+                    return null;
+                }
+            }
             $dates = $node->filter('.footer')->innerText();
             [$dateStart, $dateEnd] = explode(' → ', trim($dates, '()'));
+            $dateTimeZone = new \DateTimeZone('Europe/Amsterdam');
 
             return new CalendarItem(
                 Uuid::v6(),
                 $title,
                 '',
-                (new \DateTimeImmutable($dateStart))->setTime(0, 0, 0),
-                (new \DateTimeImmutable($dateEnd))->setTime(0, 0, 0),
+                (new \DateTimeImmutable($dateStart, $dateTimeZone)),
+                (new \DateTimeImmutable($dateEnd, $dateTimeZone))
             );
         });
+
+        $events = array_filter($events);
 
         return new Calendar('Pokemon Go', $events);
     }
