46d2e9f94925941d8572b98bee34193adf17e35d
Jeroen van den Enden <info@endroid.nl>
2023-06-06 14:38:55 +0200

    fix: include main events

--- src/Client/MunchlaxClient.php
+++ src/Client/MunchlaxClient.php
@@ -12,10 +12,40 @@ final class MunchlaxClient
     public function getCalendar(): Calendar
     {
         $skipTitles = ['Season ', ' Special Research'];
-        $skipCategories = ['Go Battle League', 'Research Breakthrough', 'Research Breaktrough'];
+        $skipCategories = ['Go Battle League', 'Research Break'];
 
         $crawler = new Crawler((string) file_get_contents('https://www.munchlax.nl/'));
-        $events = $crawler->filter('.list-group-item')->each(function (Crawler $node) use ($skipTitles, $skipCategories) {
+
+        $liveEvents = $crawler->filter('.card-live a')->each(function (Crawler $node) use ($skipTitles, $skipCategories) {
+            $liveEventUrl = 'https://www.munchlax.nl/'.$node->attr('href');
+            $liveEventCrawler = new Crawler((string) file_get_contents($liveEventUrl));
+            $title = $liveEventCrawler->filter('h1')->innerText();
+            foreach ($skipTitles as $skipTitle) {
+                if (str_contains($title, $skipTitle)) {
+                    return null;
+                }
+            }
+            $category = $liveEventCrawler->filter('.badge-primary')->innerText();
+            foreach ($skipCategories as $skipCategory) {
+                if (strtolower($skipCategory) === strtolower($category)) {
+                    return null;
+                }
+            }
+
+            $dates = $liveEventCrawler->filter('.sub')->innerText();
+            [$dateStart, $dateEnd] = explode(' → ', trim($dates, '()'));
+            $dateTimeZone = new \DateTimeZone('Europe/Amsterdam');
+
+            return new CalendarItem(
+                Uuid::v6(),
+                $title,
+                $liveEventUrl,
+                \DateTimeImmutable::createFromFormat('d-m-y H:i', $dateStart, $dateTimeZone),
+                \DateTimeImmutable::createFromFormat('d-m-y H:i', $dateEnd, $dateTimeZone)
+            );
+        });
+
+        $otherEvents = $crawler->filter('.list-group-item')->each(function (Crawler $node) use ($skipTitles, $skipCategories) {
             $title = $node->filter('.title')->innerText();
             foreach ($skipTitles as $skipTitle) {
                 if (str_contains($title, $skipTitle)) {
@@ -37,11 +67,12 @@ final class MunchlaxClient
                 Uuid::v6(),
                 $title,
                 $link,
-                (new \DateTimeImmutable($dateStart, $dateTimeZone)),
-                (new \DateTimeImmutable($dateEnd, $dateTimeZone))
+                \DateTimeImmutable::createFromFormat('d-m-Y H:i', $dateStart, $dateTimeZone),
+                \DateTimeImmutable::createFromFormat('d-m-Y H:i', $dateEnd, $dateTimeZone)
             );
         });
 
+        $events = array_merge($liveEvents, $otherEvents);
         $events = array_filter($events);
 
         return new Calendar('Pokemon Go', $events);
